4d35034b23cd888b8d09f11a91df3fc0c1043e1c
qvixote <fajar2691@gmail.com>
2023-07-04 14:04:08 +0700

    Second commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,68 @@
 
+// TEST FORM 1
+
+
+const form = document.getElementById('form');
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const phonenumber = document.getElementById('phonenumber');
+
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    validateInputs();
+});
+
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success')
+}
+
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+};
+
+const isValidEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+const validateInputs = () => {
+    const nameValue = name.value.trim();
+    const emailValue = email.value.trim();
+    const phonenumberValue = phonenumber.value.trim();
+
+
+    if(nameValue === '') {
+        setError(name, 'Name is required');
+    } else {
+        setSuccess(name);
+    }
+
+    if(emailValue === '') {
+        setError(email, 'Email is required');
+    } else if (!isValidEmail(emailValue)) {
+        setError(email, 'Provide a valid email address');
+    } else {
+        setSuccess(email);
+    }
+
+    if(phonenumberValue === '') {
+        setError(phonenumber, 'Phone number is required');
+    } else if (phonenumberValue.length < 11 || phonenumberValue.length > 13) {
+        setError(phonenumber, 'Phone Number invalid.')
+    } else {
+        setSuccess(phonenumber);
+    }
+
+};
