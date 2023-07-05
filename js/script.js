d61bf07600a82812b9bb94da1413b06c949d20c7
qvixote <fajar2691@gmail.com>
2023-07-05 10:56:56 +0700

    add auto slide

--- js/script.js
+++ js/script.js
@@ -66,3 +66,36 @@ const validateInputs = () => {
     }
 
 };
+
+
+// AUTO SLIDE
+
+var i = 0; 			// Start Point
+var images = [];	// Images Array
+var time = 3000;	// Time Between Switch
+	 
+// Image List
+images[0] = "img/Slide/image1.jpg";
+images[1] = "img/Slide/image2.jpg";
+images[2] = "img/Slide/image3.jpg";
+images[3] = "img/Slide/image4.jpg";
+
+// Change Image
+function changeImg(){
+	document.slide.src = images[i];
+
+	// Check If Index Is Under Max
+	if(i < images.length - 1){
+	  // Add 1 to Index
+	  i++; 
+	} else { 
+		// Reset Back To O
+		i = 0;
+	}
+
+	// Run function every x seconds
+	setTimeout("changeImg()", time);
+}
+
+// Run function when page loads
+window.onload=changeImg;
